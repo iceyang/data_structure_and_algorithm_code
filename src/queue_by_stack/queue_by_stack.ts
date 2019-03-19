eabffcc7998542c89e3260594e442c264223c77e
Justin <iceyang1007@gmail.com>
2019-03-19 22:53:53 +0800

    增加由栈实现的队列优化版

--- src/queue_by_stack/queue_by_stack.ts
+++ src/queue_by_stack/queue_by_stack.ts
@@ -1,9 +1,18 @@
 /**
  * 由栈实现的队列
+ * 只保存一个栈，入队时，直接进栈，出队时，需要先将栈反转，出栈后再反转回来
  */
 import Stack from '../lib/stack';
 
-export default class QueueByStack<T> {
+export interface Queue<T> {
+  length(): number;
+
+  push(t: T): void;
+  
+  poll(): T | undefined;
+}
+
+export class QueueByStack<T> implements Queue<T> {
   private stack: Stack<T> = new Stack<T>();
 
   length(): number { return this.stack.length(); }
@@ -23,10 +32,35 @@ export default class QueueByStack<T> {
   }
 
   poll(): T | undefined {
-    if (this.stack.length() <= 0) return undefined;
+    if (this.length() <= 0) return undefined;
     this.reverseStack();
     const result = this.stack.pop();
     this.reverseStack();
     return result;
+  }
+}
+
+/**
+ * 优化版本，保存两个栈，一个用于入队(S1)，一个用于出队(S2)。
+ * 入队时：直接进栈S1
+ * 出队时：假如S2为空，则将S1倒入S2中，S2出栈；如果S2不为空，则S2直接出栈
+ */
+export class QueueByStack2<T> implements Queue<T>{
+  private s1: Stack<T> = new Stack<T>();
+  private s2: Stack<T> = new Stack<T>();
+
+  length(): number { return this.s1.length() + this.s2.length(); }
+
+  push(t: T) {
+    this.s1.push(t);
+  }
+
+  poll(): T | undefined {
+    if (this.length() <= 0) return undefined;
+    if (this.s2.length() > 0) return this.s2.pop();
+    while (this.s1.length() > 0) {
+      this.s2.push(<T>this.s1.pop());
+    }
+    return this.s2.pop();
   }
 }
--- src/queue_by_stack/queue_by_stack_test.ts
+++ src/queue_by_stack/queue_by_stack_test.ts
@@ -1,14 +1,18 @@
-import Queue from './queue_by_stack';
+import { Queue, QueueByStack, QueueByStack2 } from './queue_by_stack';
 import { expect } from 'chai';
 
-const queue = new Queue<number>();
+function testQueue(queue: Queue<number>) {
+  queue.push(1);
+  queue.push(2);
+  queue.push(3);
 
-queue.push(1);
-queue.push(2);
-queue.push(3);
+  expect(queue.poll()).to.equal(1);
+  expect(queue.poll()).to.equal(2);
 
-expect(queue.poll()).to.equal(1);
-expect(queue.poll()).to.equal(2);
+  queue.push(4);
+  expect(queue.poll()).to.equal(3);
+  expect(queue.poll()).to.equal(4);
+}
 
-queue.push(4);
-expect(queue.poll()).to.equal(3);
+testQueue(new QueueByStack<number>());
+testQueue(new QueueByStack2<number>());
